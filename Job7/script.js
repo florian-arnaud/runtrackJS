ea74162da569d7ccbdc4c95681c97560bb42a2a1
phaeryl <florian.arnaud2019@gmail.com>
2024-03-25 11:51:24 +0100

    Week-end condition done

--- Job7/script.js
+++ Job7/script.js
@@ -1,48 +1,24 @@
 function jourTravaille(date)
 {
-    let dayName = "";
-    let dayMonth = "";
-    let dayNumber = "";
-    let answer = "";
-    let dateText = "";
+let dayName = ""
 
 
-    switch (date.getDay) {
-        case 0:
-            dayName = "Dimanche";
-            break;
-        case 1:
-            dayName = "Lundi";
-            break;
-        case 2:
-            dayName = "Mardi";
-            break;
-        case 3:
-            dayName = "Mercredi";
-        case 4:
-            dayName = "Jeudi";
-        case 5:
-            dayName = "Vendredi";
-        case 6:
-            dayName = "Samedi";
 
-
-        default:
-            break;
-    }
-    if (date.getDay() == 0 || date.getDay() == 6)
+    let days = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+    let months = ["Janvier", "Février", "Mars", "Avril", "Mai", "Juin", "Juillet", "Août", "Septembre", "Octobre", "Novembre", "Décembre"]
+    let dateText = days[date.getDay()] + " " + date.getDate() + " " + months[date.getMonth()]
+    console.log(dateText)
+    /* Conditions */
+    if (date.getDay() === 0 || date.getDay() === 6)
     {
         let answer = "Non, le " + dateText + " est un week-end";
         return answer
     }
-       dateText = dayName + " " + dayNumber + " " + dayMonth;
+    
 
-    console.log(date)
-    console.log(date.getDay())
-    console.log(dateText)
 }
 let myDate = new Date;
 myDate.setFullYear(2024);
-myDate.setMonth(3);
-myDate.setDate(6);
+myDate.setMonth(2);
+myDate.setDate(24);
 console.log(jourTravaille(myDate));
