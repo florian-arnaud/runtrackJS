1dba7b3cab3b277da055bf289d68c39a643909ba
phaeryl <florian.arnaud2019@gmail.com>
2024-03-25 10:08:44 +0100

    Job  done

---
+++ Job6/script.js
@@ -0,0 +1,29 @@
+function fizzbuzz()
+{
+
+    for (let i = 1; i <= 151; i++) {
+        switch (true) {
+            case i % 5 == 0 && i % 3 == 0:
+                console.log(i + ": FizzBuzz");
+                break;
+                
+            case i % 3 == 0:
+                console.log( i + ": Fizz");
+                break;
+            case i % 5 == 0:
+                console.log(i + ": Buzz");
+                break
+            
+
+            default:
+                console.log(i);
+                break;
+        }
+        
+    }
+    }
+
+
+
+fizzbuzz();
+
