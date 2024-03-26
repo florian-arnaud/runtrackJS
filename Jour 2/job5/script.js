de9069e1486221881e92c017b797c3b8b79e8928
phaeryl <florian.arnaud2019@gmail.com>
2024-03-26 11:57:20 +0100

    Job 5 done

---
+++ Jour 2/job5/script.js
@@ -0,0 +1,8 @@
+let footerElement = document.getElementsByTagName("footer")[0]
+console.log(footerElement.style.width)
+document.addEventListener("scroll", (event) => {
+
+      footerElement.style.width = window.scrollX +"px"
+
+      
+});
