096a634d63effb3cec00878160a4ba6e9c13fd1a
phaeryl <florian.arnaud2019@gmail.com>
2024-03-28 10:23:35 +0100

    Job 2 finished

---
+++ Jour 4/job2/script.js
@@ -0,0 +1,15 @@
+
+const formation = {
+  "name": "La Plateforme_",
+  "address": "8 rue d'hozier",
+  "city": "Marseille",
+  "nb_staff":"11",
+  "creation": "2019"
+};
+
+function jsonValueKey(json, key) {   
+  const keyValue = json[key]; 
+  console.log(keyValue); 
+}
+
+jsonValueKey(formation, "city")
